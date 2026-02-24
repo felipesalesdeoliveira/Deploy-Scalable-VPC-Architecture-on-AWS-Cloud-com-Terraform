e3afb17e9388b9e6e0480846bc23173e657ad607
felipesalesoliveira <fesales.oliveira@gmail.com>
2026-02-24 14:43:32 -0300

    feat: update prod environment

--- aws-vpc-scalable-architecture/environments/prod/main.tf
+++ aws-vpc-scalable-architecture/environments/prod/main.tf
@@ -1,94 +1,122 @@
+locals {
+  project = "scalable-vpc"
+  env     = "dev"
+
+  common_tags = {
+    Project = local.project
+    Env     = local.env
+    Owner   = "Felipe"
+  }
+}
 ###############################################################################
 # VPC BASTION & APP
 ###############################################################################
 module "vpc_bastion" {
-  source = "../../modules/vpc"
+  source = "../../modules/networking/vpc"
 
-  name = "bastion-vpc"
-  cidr = "192.168.0.0/16"
+  name                 = "bastion-vpc"
+  cidr                 = "192.168.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags                 = local.common_tags
 }
 
 module "vpc_app" {
-  source = "../../modules/vpc"
+  source = "../../modules/networking/vpc"
 
-  name = "app-vpc"
-  cidr = "172.32.0.0/16"
+  name                 = "app-vpc"
+  cidr                 = "172.32.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags                 = local.common_tags
 }
 ###############################################################################
 # SUBNETS PUBLIC & PRIVATE
 ###############################################################################
 module "bastion_public_subnets" {
-  source = "../../modules/subnets"
+  source = "../../modules/networking/subnets"
 
   name   = "bastion-public"
   vpc_id = module.vpc_bastion.vpc_id
 
-  cidrs = ["192.168.1.0/24"]
-  azs   = ["us-east-1a"]
-
+  cidrs  = ["192.168.1.0/24"]
+  azs    = ["us-east-1a"]
   public = true
+
+  tags = local.common_tags
 }
+
 module "app_public_subnets" {
-  source = "../../modules/subnets"
+  source = "../../modules/networking/subnets"
 
   name   = "app-public"
   vpc_id = module.vpc_app.vpc_id
 
-  cidrs = ["172.32.1.0/24", "172.32.2.0/24"]
-  azs   = ["us-east-1a", "us-east-1b"]
-
+  cidrs  = ["172.32.1.0/24", "172.32.2.0/24"]
+  azs    = ["us-east-1a", "us-east-1b"]
   public = true
+
+  tags = local.common_tags
 }
+
 module "app_private_subnets" {
-  source = "../../modules/subnets"
+  source = "../../modules/networking/subnets"
 
   name   = "app-private"
   vpc_id = module.vpc_app.vpc_id
 
-  cidrs = ["172.32.10.0/24", "172.32.11.0/24"]
-  azs   = ["us-east-1a", "us-east-1b"]
-
+  cidrs  = ["172.32.10.0/24", "172.32.11.0/24"]
+  azs    = ["us-east-1a", "us-east-1b"]
   public = false
+
+  tags = local.common_tags
 }
 ###############################################################################
 # INTERNET GATEWAY 
 ###############################################################################
 module "bastion_igw" {
-  source = "../../modules/internet-gateway"
+  source = "../../modules/networking/internet-gateway"
 
   name   = "bastion-igw"
   vpc_id = module.vpc_bastion.vpc_id
+  tags   = local.common_tags
 }
+
 module "app_igw" {
-  source = "../../modules/internet-gateway"
+  source = "../../modules/networking/internet-gateway"
 
   name   = "app-igw"
   vpc_id = module.vpc_app.vpc_id
+  tags   = local.common_tags
 }
 ###############################################################################
 # ROUTE TABLES 
 ###############################################################################
 module "bastion_public_rt" {
-  source = "../../modules/route-tables"
+  source = "../../modules/networking/route-tables"
 
-  name   = "bastion-public-rt"
-  vpc_id = module.vpc_bastion.vpc_id
-
+  name       = "bastion-public-rt"
+  vpc_id     = module.vpc_bastion.vpc_id
   subnet_ids = module.bastion_public_subnets.subnet_ids
 
   routes = [
     {
       cidr_block = "0.0.0.0/0"
       gateway_id = module.bastion_igw.igw_id
+    },
+    {
+      cidr_block         = "172.32.0.0/16"
+      transit_gateway_id = module.transit_gateway.tgw_id
     }
   ]
+
+  tags = local.common_tags
 }
 module "app_public_rt" {
-  source = "../../modules/route-tables"
+  source = "../../modules/networking/route-tables"
 
-  name   = "app-public-rt"
-  vpc_id = module.vpc_app.vpc_id
-
+  name       = "app-public-rt"
+  vpc_id     = module.vpc_app.vpc_id
   subnet_ids = module.app_public_subnets.subnet_ids
 
   routes = [
@@ -97,36 +125,44 @@ module "app_public_rt" {
       gateway_id = module.app_igw.igw_id
     }
   ]
+
+  tags = local.common_tags
 }
 module "app_private_rt" {
-  source = "../../modules/route-tables"
+  source = "../../modules/networking/route-tables"
 
-  name   = "app-private-rt"
-  vpc_id = module.vpc_app.vpc_id
-
+  name       = "app-private-rt"
+  vpc_id     = module.vpc_app.vpc_id
   subnet_ids = module.app_private_subnets.subnet_ids
 
   routes = [
     {
       cidr_block     = "0.0.0.0/0"
       nat_gateway_id = module.app_nat.nat_gateway_id
+    },
+    {
+      cidr_block         = "192.168.0.0/16"
+      transit_gateway_id = module.transit_gateway.tgw_id
     }
   ]
+
+  tags = local.common_tags
 }
 ###############################################################################
 # NAT GATEWAY 
 ###############################################################################
 module "app_nat" {
-  source = "../../modules/nat-gateway"
+  source = "../../modules/networking/nat-gateway"
 
   name      = "app-nat"
   subnet_id = module.app_public_subnets.subnet_ids[0]
+  tags      = local.common_tags
 }
 ###############################################################################
 # TRANSIT GATEWAY 
 ###############################################################################
 module "transit_gateway" {
-  source = "../../modules/transit-gateway"
+  source = "../../modules/networking/transit-gateway"
 
   name = "main-tgw"
 
@@ -142,29 +178,33 @@ module "transit_gateway" {
       subnet_ids = module.app_private_subnets.subnet_ids
     }
   ]
+
+  tags = local.common_tags
 }
 ###############################################################################
 # BASTION HOST
 ###############################################################################
 data "aws_ami" "amazon_linux" {
   most_recent = true
-
-  owners = ["amazon"]
+  owners      = ["amazon"]
 
   filter {
     name   = "name"
     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
 }
+
 module "bastion_host" {
   source = "../../modules/compute/bastion"
 
-  name               = "bastion-host"
-  ami_id             = data.aws_ami.amazon_linux.id
-  instance_type      = "t3.micro"
-  subnet_id          = module.bastion_public_subnets.subnet_ids[0]
-  security_group_ids = [module.bastion_sg.security_group_id]
-  key_name           = "sua-keypair"
-  associate_public_ip = true
+  name                = "bastion-host"
+  ami_id              = data.aws_ami.amazon_linux.id
+  instance_type       = "t3.micro"
+  subnet_id           = module.bastion_public_subnets.subnet_ids[0]
+  security_group_ids  = [module.bastion_sg.security_group_id]
+  key_name            = "sua-keypair"
+  associate_public_ip = false
   allocate_eip        = true
+
+  tags = local.common_tags
 }
